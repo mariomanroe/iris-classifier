ad091f1913a64f1acb3b6ff6ff0637f1ee9ebbb5
Mario Desendi <105722129+mariomanroe@users.noreply.github.com>
2025-06-12 01:24:58 +0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,10 @@
+import os
 import pickle
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
+
+# Buat folder model jika belum ada
+os.makedirs('model', exist_ok=True)
 
 # Load dataset
 iris = load_iris()
